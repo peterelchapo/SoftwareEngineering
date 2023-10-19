9a7f62a73afa05a5f81def0db5f37025210aa0ec
Leo Wainwright <Leojpninja@gmail.com>
2023-10-19 15:27:31 +0100

    tasks

--- Tasks/05 - Functions/Functions/503-passing-parameters/503-passing-parameters.cpp
+++ Tasks/05 - Functions/Functions/503-passing-parameters/503-passing-parameters.cpp
@@ -9,23 +9,31 @@ void displayBanner(int stringLen)
 	cout << endl;
 }
 
-int main()
-{
-	const string message = "Welcome to Computer Science";
 
-	//Get the length of the string
-	int L = message.length();			//L is now a LOCAL variable
+void DisplayWithBanner(string z) {
+
+	int L = z.length();			//L is now a LOCAL variable
 
 	//Write the banner above
 	displayBanner(L);
 
 	cout << "* ";				//Leading *
-	cout << message;			//Message
+	cout << z;			//Message
 	cout << " *" << endl;		//Trailing *
-
-	//Write the banner below
 	displayBanner(L);
+}
+
+
+int main()
+{
+	const string message = "Welcome to Computer Science";
+
+	//Get the length of the string
+	
+	//Write the banner below
+	DisplayWithBanner(message);
 
 	//Tell the calling shell all is well
 	return 0;
 }
+
